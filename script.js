207bba76eeb753ee138bc87521f194ee981ebc96
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-06-03 15:48:22 -0400

    attempt at X and O text unsuccessful

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
-const gameBtn = document.querySelector('button');
-const gameLayout = document.querySelector('.game-layout');
-const editPlayer1 = document.querySelector('.edit-player-1');
-const editPlayer2 = document.querySelector('.edit-player-2');
-const player1Name = document.querySelector('.player-1-name');
-const player2Name = document.querySelector('.player-2-name');
-const currentPlayer = document.querySelector('.current-player');
-const gameBoard = document.querySelectorAll('.game-board > div');
+const gameBtn = document.querySelector("button");
+const gameLayout = document.querySelector(".game-layout");
+const editPlayer1 = document.querySelector(".edit-player-1");
+const editPlayer2 = document.querySelector(".edit-player-2");
+const player1Name = document.querySelector(".player-1-name");
+const player2Name = document.querySelector(".player-2-name");
+const currentPlayer = document.querySelector(".current-player");
+const gameBoard = document.querySelectorAll(".game-board > div");
 let player1;
 let player2;
 
-gameBoard.forEach(function(i) {
-  i.addEventListener('click', chooseTile);
+gameBoard.forEach(function (i) {
+  i.addEventListener("click", chooseTile);
 });
-editPlayer1.addEventListener('click', editName);
-editPlayer2.addEventListener('click', editName);
-gameBtn.addEventListener('click', startGame);
+editPlayer1.addEventListener("click", editName);
+editPlayer2.addEventListener("click", editName);
+gameBtn.addEventListener("click", startGame);
 
 function editName(event) {
   switch (event.target) {
-  case editPlayer1:
-    player1 = prompt('Enter a Username for Player 1', '');
-    player1Name.textContent = player1;
-    break;
-  case editPlayer2:
-    player2 = prompt('Enter a Username for Player 2', '');
-    player2Name.textContent = player2;
-    break;
-}}
+    case editPlayer1:
+      player1 = prompt("Enter a Username for Player 1", "");
+      player1Name.textContent = player1;
+      break;
+    case editPlayer2:
+      player2 = prompt("Enter a Username for Player 2", "");
+      player2Name.textContent = player2;
+      break;
+  }
+}
 
 function startGame() {
-  gameLayout.style.display = 'block';
+  gameLayout.style.display = "block";
   currentPlayer.textContent = player1;
-  if (gameLayout.style.display = 'block') {
-    gameBtn.textContent = 'Reset Game?';
+  if ((gameLayout.style.display = "block")) {
+    gameBtn.textContent = "Reset Game?";
     resetGame();
   }
 }
 
 function chooseTile(event) {
-  event.target.setAttribute('class', 'chosen');
+  event.target.setAttribute("class", "chosen");
   updatePlayerDisplay();
+  updateGameTile();
 }
 
 function resetGame() {
-  gameBoard.forEach(function(i) {
-    i.setAttribute('class', '');
-  })
+  gameBoard.forEach(function (i) {
+    i.setAttribute("class", "");
+  });
+}
+
+function updateGameTile(gameBoard) {
+  console.log("click");
+  gameBoard.textContent = "O";
+  switch (currentPlayer.textContent) {
+    case player1:
+      console.log("Player1 turn");
+      gameBoard.setAttribute("text", "X");
+      break;
+    case player2:
+      console.log("Player2 turn");
+      gameBoard.textContent = "O";
+      break;
+  }
 }
 
 function updatePlayerDisplay() {
@@ -53,7 +70,7 @@ function updatePlayerDisplay() {
     case player1:
       currentPlayer.textContent = player2;
       break;
-    case player2: 
+    case player2:
       currentPlayer.textContent = player1;
       break;
   }
